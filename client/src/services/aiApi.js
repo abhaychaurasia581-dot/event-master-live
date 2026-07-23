e8c6ee5b0070ea0de5c6c0c53d6df9388457a157
abhay <abhaychaurasia581@gmail.com>
2026-07-23 13:04:59 +0530

    Fix AI API route prefix

--- client/src/services/aiApi.js
+++ client/src/services/aiApi.js
@@ -2,7 +2,7 @@ import api from './api';
 
 export const aiApi = {
   getRecommendations: async (params = {}) => {
-    const response = await api.get('/ai/recommendations', { params });
+    const response = await api.get('/api/v2/ai/recommendations', { params });
     return response.data;
   }
 };
